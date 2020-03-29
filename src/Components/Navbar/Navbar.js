d3d33637c416d390f77ce60af9ae36f23c9f6a0f
audreyhou <audrey.artncreation@gmail.com>
2020-03-28 22:17:30 -0700

    changed  navbar, need to test

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,59 +1,184 @@
 import React, { useState } from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { 
+  Drawer,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  List,
+  Typography,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core';
+import { 
+  MenuIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon
+} from '@material-ui/icons';
 import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
+  Container,
+  Col,
+  Row
 } from 'reactstrap';
+import './Navbar.css'
+
+const drawerWidth = '100vw';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
 
 function NavBar({
   component: Component
 }) {
-  const [isOpen, setIsOpen] = useState(false);
+  const classes = useStyles();
+  const theme = useTheme();
+  const [isOpen, setIsOpen] = React.useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+  const drawerOpen = () => {
+    setIsOpen(true);
+  };
+
+  const drawerClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
-      <Navbar color='light' light expand='md'>
-        <NavbarBrand href='/'>Crimes Against Asians</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className='mr-auto' navbar>
-            <NavItem>
-              <NavLink href='/'>Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='/stories'>Stories</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='/news'>News</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Statistics
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem href='/charts'>
-                  Charts and Graphs
-                </DropdownItem>
-                <DropdownItem href='/map'>
-                  Map
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
-      <Component />
+      <Container fluid style={{ paddingLeft: '0px', paddingRight: '0px', overflow:'hidden' }}>
+        <Row>
+          <Col md='3' style={{overflow:'hidden', height:'100vh'}
+            }>
+            {/* <Navbar light expand=''>
+              <NavbarToggler onClick={toggle} />
+              <Collapse isOpen={isOpen} navbar>
+                <Nav tabs vertical>
+                  <NavItem>
+                    <NavLink href='/'>Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href='/posts'>Posts</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href='/stats'>Statistics</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar> */}
+            <Drawer
+              className={classes.drawer}
+              variant="persistent"
+              anchor="left"
+              open={isOpen}
+              classe={{
+                paper: classes.drawerPaper
+              }}
+            >
+              <div className={classes.drawerHeader}>
+                <IconButton onClick={drawerClose}>
+                  {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+              </div>
+              <List>
+                {['Home'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+              <Divider />
+              <List>
+                {['Posts', 'Statistics'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+              <Divider />
+              <List>
+                {['About Us'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
+            <main
+              className={clsx(classes.content, {
+                [classes.contentShift]: isOpen,
+              })}
+            >
+
+            </main>
+          </Col>
+          <Col>
+            <Component />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
