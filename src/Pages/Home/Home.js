e946c07af64ee41aae1e620effd58e0bb57fc854
jerrylee17 <jerrylee1157@gmail.com>
2020-03-29 18:10:53 -0700

    Made Home.js a function instead of a class

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Home.css'
 
-class Home extends Component {
-  render() {
-    return (
-      <>
-        <div className='homeTitle'>
-          <h1>Crimes Against Asians</h1>
-          <br />
-          <h3> About this platform </h3>
-          The purpose of this website is to provide a platform for people 
-          to see the racism that Asians face. This website will pull 
-          data from different sources and sort the data into different 
+export default function Home() {
+  return (
+    <>
+      <div className='homeTitle'>
+        <h1>Crimes Against Asians</h1>
+        <br />
+        <h3> About this platform </h3>
+          The purpose of this website is to provide a platform for people
+          to see the racism that Asians face. This website will pull
+          data from different sources and sort the data into different
           categories, allowing news reporters and journalists to capture data easier.
         </div>
-      </>
-    );
-  }
+    </>
+  );
 }
-
-export default Home;
