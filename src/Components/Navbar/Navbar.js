ae8095ecdf655febe97612994f0148229bf9ea0f
cianyyz <cian.kelleher33@gmail.com>
2020-04-05 16:41:21 -0400

    master fix

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,10 +5,7 @@ import {
   NavbarToggler,
   Nav,
   NavItem,
-  NavLink,
-  Container,
-  Col,
-  Row
+  NavLink
 } from 'reactstrap';
 import './Navbar.css'
 
@@ -18,37 +15,28 @@ function NavBar({
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
-  const containerStyles={ paddingLeft: '0px', paddingRight: '0px', overflow:'hidden' };
 
   return (
     <>
-      <Container fluid style={containerStyles}>
-        <Row>
-          <Col md='3' style={
-            {overflow:'hidden', height:'100vh', backgroundColor:'blue'}
-            }>
-            <Navbar light expand=''>
-              <NavbarToggler onClick={toggle} />
-              <Collapse isOpen={isOpen} navbar>
-                <Nav tabs vertical>
-                  <NavItem>
-                    <NavLink href='/'>Home</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href='/posts'>Posts</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href='/stats'>Statistics</NavLink>
-                  </NavItem>
-                </Nav>
-              </Collapse>
-            </Navbar>
-          </Col>
-          <Col style={containerStyles}>
-            <Component />
-          </Col>
-        </Row>
-      </Container>
+      <div>
+        <Navbar sticky='left' light expand=''>
+        <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav tabs vertical>
+              <NavItem>
+                <NavLink href='/'>Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='/posts'>Posts</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='/stats'>Statistics</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+      <Component/>
     </>
   );
 }
