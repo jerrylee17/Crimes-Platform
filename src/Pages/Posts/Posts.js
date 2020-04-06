f845ac609f85e2ca97eac46173e413ec628bf288
Jerry Lee <jerrylee1157@gmail.com>
2020-04-05 22:30:15 -0700

    added frontend tab template

--- src/Pages/Posts/Posts.js
+++ src/Pages/Posts/Posts.js
@@ -1,52 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
+import classnames from 'classnames';
+import Tab1 from './Tab1';
 
-import PostList from '../../Components/post/PostList';
-import ErrorModal from '../../Components/UIComponents/ErrorModal';
-import LoadingSpinner from '../../Components/UIComponents/LoadingSpinner';
+/**
+ * @param {json} props contains all of the information in a post
+ * @returns layout of tabs
+ */
+export default function Post() {
+  const [activeTab, setActiveTab] = useState('1');
 
-const Posts = () => {
-  const [loadedPosts, setLoadedPosts] = useState();
-  // const { isLoading, error, sendRequest, clearError } = useHttpClient();
-
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     try {
-  //       const responseData = await sendRequest(
-  //         `http://localhost:5000/posts`
-  //       );
-  //       setLoadedPosts(responseData.posts);
-  //     } catch (err) {}
-  //   };
-  //   fetchPosts();
-  // }, [sendRequest]);
-
-  const DUMMYYPOSTS = [
-    {
-      id: '1',
-      image: 'https://i.kym-cdn.com/entries/icons/original/000/022/134/elmo.jpg',
-      content: 'boop'
-    },
-    {
-      id: '2',
-      image: 'https://i.kym-cdn.com/entries/icons/original/000/022/134/elmo.jpg',
-      content: 'boop numero dos'
-    }
-  ];
+  const toggle = tab => {
+    if (activeTab !== tab) setActiveTab(tab);
+  };
 
   return (
-    <React.Fragment>
-      {/* <ErrorModal error={error} onClear={clearError} />
-      {isLoading && (
-        <div className="center">
-          <LoadingSpinner />
-        </div>
-      )}
-      {!isLoading && loadedPlaces && ( */}
-      <PostList items={DUMMYYPOSTS} />
-      {/* )} */}
-    </React.Fragment>
-  );
-};
+    <>
+      <Nav tabs>
+        <Container>
+          <Row>
+            <Col>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === '1' })}
+                  onClick={() => toggle('1')}>
+                  Stories
+                </NavLink>
+              </NavItem>
+            </Col>
+            <Col>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === '2' })}
+                  onClick={() => toggle('2')}>
+                  News
+                </NavLink>
+              </NavItem>
+            </Col>
+            <Col>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === '3' })}
+                  onClick={() => toggle('3')}>
+                  Misc
+                </NavLink>
+              </NavItem>
+            </Col>
+          </Row>
+        </Container>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId='1'>
+          <Tab1 />
+        </TabPane>
+        <TabPane tabId='2'>
+          <h4>Render tab 2 here</h4>
+        </TabPane>
+        <TabPane tabId='3'>
+          <h4>Render tab 3 here</h4>
 
-export default Posts;
+        </TabPane>
+      </TabContent>
+    </>
+  );
+}
