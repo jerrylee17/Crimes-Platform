95d714a6a872ef07c1ee8172575f2e68ed0b4301
Jerry Lee <jerrylee1157@gmail.com>
2020-04-05 22:39:21 -0700

    added frontend tab template (#18)

---
+++ src/Components/UIComponents/Tab.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Card,
+  CardTitle,
+  CardText,
+  CardImg,
+  Button
+} from 'reactstrap';
+import './Tab.css';
+
+
+export default function Tab(props) {
+  const {
+    name,
+    posts
+  } = props;
+  console.log(posts);
+
+  return (
+    <>
+      <br />
+      <h1> {name}</h1>
+      <br />
+      <Container fluid>
+        <section className='grid'>
+          {posts && posts.length ? (
+            posts.map((object, index) => {
+              return (
+                <Card>
+                  <CardTitle className='text-center'>
+                    Post #{index}
+                  </CardTitle>
+                  <CardText>
+                    {object.content}
+                    {object.content.split(' ').slice(0, 30).join(' ')}
+                  </CardText>
+                </Card>
+              );
+            })
+          ) : (
+            <h1>No posts yet!</h1>
+          )}
+        </section>
+      </Container>
+    </>
+  );
+}
