36aa7fb781287e08f50d98d125a69c273993ce53
Jerry Lee <jerrylee1157@gmail.com>
2020-04-06 16:00:11 -0700

    Set up a bad display toggler. Functionality works though. (#23)

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -21,46 +21,12 @@ function NavBar({
       <Container fluid style={containerStyles}>
         {isOpen && <BackDrop onClick={toggle} />}
 
-        {/* <button
-          onClick={toggle}
-        >
-          <span style={color='white'}/>
-          <span style={color='white'}/>
-          <span style={color='white'}/>
-        </button> */}
         <NavbarToggler onClick={toggle} />
         <SideDrawer show={isOpen} onClick={toggle}>
           <nav className='main-navigation__drawer-nav'>
             <NavLinks />
           </nav>
         </SideDrawer>
-
-        {/* <Col style={containerStyles}>
-        <Component />
-      </Col>  */}
-        {/*<Row>
-          <Col md='3' style={
-            {overflow:'hidden', height:'100vh', backgroundColor:'blue'}
-            }>
-            <Navbar light expand=''>
-              <NavbarToggler onClick={toggle} />
-              <Collapse isOpen={isOpen} navbar>
-                <Nav tabs vertical>
-                  <NavItem>
-                    <NavLink href='/'>Home</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href='/posts'>Posts</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink href='/stats'>Statistics</NavLink>
-                  </NavItem>
-                </Nav>
-              </Collapse>
-            </Navbar>
-          </Col>
-          
-          </Row>*/}
 
       </Container>
     </>
