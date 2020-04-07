1339fe96af0538720f344d5826c61d810346fd7a
Jerry Lee <jerrylee1157@gmail.com>
2020-04-06 21:34:24 -0700

    Refactored the display tab code so it is not hardcoded (#26)

--- src/Pages/Posts/Posts.js
+++ src/Pages/Posts/Posts.js
@@ -1,17 +1,57 @@
 import React, { useState } from 'react';
 import { Button } from 'reactstrap';
-import Disp1 from './Disp1';
-import Disp2 from './Disp2';
+import Display from '../../Components/PostsDisplay/Display';
+import Attacks from './Disp2/Attacks';
+import Hate from './Disp2/Hate';
+import Vandalism from './Disp2/Vandalism';
+import Stories from './Disp1/Stories.js';
+import News from './Disp1/News.js';
+import Misc from './Disp1/Misc.js';
 
 export default function Posts() {
   const [disp, toggleDisp] = useState(true);
+  const Disp1 = {
+    tabs: [{
+      title: 'Stories',
+      Component: Stories
+    },
+    {
+      title: 'News',
+      Component: News
+    },
+    {
+      title: 'Misc',
+      Component: Misc
+    },
+    ]
+  };
+  const Disp2 = {
+    tabs: [{
+      title: 'Hate Speech',
+      Component: Hate
+    },
+    {
+      title: 'Vandalism',
+      Component: Vandalism
+    },
+    {
+      title: 'Attacks',
+      Component: Attacks
+    },
+    {
+      title: 'Misc',
+      Component: Misc
+    }
+    ]
+  };
+
   return (
     <div>
       <Button onClick={() => toggleDisp(!disp)} > Click to toggle displays </Button>
       {disp ? (
-        <Disp1 />
+        <Display {...Disp1} />
       ) : (
-        <Disp2 />
+        <Display {...Disp2} />
       )}
     </div>
   );
