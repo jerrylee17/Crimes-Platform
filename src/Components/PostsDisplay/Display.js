fbde42334b45665f9f9c4f52059326b8e1ce3449
Jerry Lee <jerrylee1157@gmail.com>
2020-04-06 21:26:33 -0700

    Refactored the display tab code so it is not hardcoded

---
+++ src/Components/PostsDisplay/Display.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
+import classnames from 'classnames';
+
+export default function Display(props) {
+  const [activeTab, setActiveTab] = useState('1');
+
+  const toggle = tab => {
+    if (activeTab !== tab) setActiveTab(tab);
+  };
+  const tabs = props.tabs;
+
+  return (
+    <>
+      <Nav tabs>
+        <Container>
+          <Row>
+            {tabs.map((Tab, index) => {
+              return (
+                <Col>
+                  <NavItem>
+                    <NavLink
+                      className={classnames({
+                        active: activeTab === String(index + 1)
+                      })}
+                      onClick={() => toggle(String(index + 1))}
+                    >
+                      {Tab.title}
+                    </NavLink>
+                  </NavItem>
+                </Col>
+              );
+            })
+            }
+          </Row>
+        </Container>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        {tabs.map((Tab, index) => {
+          return (
+            <TabPane tabId={String(index + 1)}>
+              <Tab.Component />
+            </TabPane>
+          );
+        })}
+      </TabContent>
+    </>
+  );
+}
