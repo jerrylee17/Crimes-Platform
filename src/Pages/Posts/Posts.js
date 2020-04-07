7cb766035b410024351f0db2fadc3ef191a539fa
Jerry Lee <jerrylee1157@gmail.com>
2020-04-06 22:59:38 -0700

    Fixed lint not working (#27)

--- src/Pages/Posts/Posts.js
+++ src/Pages/Posts/Posts.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 import { Button } from 'reactstrap';
 import Display from '../../Components/PostsDisplay/Display';
 import Attacks from './Disp2/Attacks';
@@ -8,23 +7,6 @@ import Vandalism from './Disp2/Vandalism';
 import Stories from './Disp1/Stories.js';
 import News from './Disp1/News.js';
 import Misc from './Disp1/Misc.js';
-=======
-import {
-  Nav,
-  NavItem,
-  NavLink,
-  TabContent,
-  TabPane,
-  Container,
-  Row,
-  Col
-} from 'reactstrap';
-import classnames from 'classnames';
-import Tab1 from './Tab1';
-import Tab from '../../Components/UIComponents/Tab';
-import './Posts.css';
-
->>>>>>> tabs and page styling for posts compkete
 
 export default function Posts() {
   const [disp, toggleDisp] = useState(true);
@@ -64,7 +46,6 @@ export default function Posts() {
   };
 
   return (
-<<<<<<< HEAD
     <div>
       <Button onClick={() => toggleDisp(!disp)} > Click to toggle displays </Button>
       {disp ? (
@@ -73,54 +54,5 @@ export default function Posts() {
         <Display {...Disp2} />
       )}
     </div>
-=======
-    <>
-      <Nav tabs>
-        <Container>
-          <Row>
-            <Col>
-              <NavItem className="tabStyle">
-                <NavLink
-                  className={classnames('Story', { active: activeTab === '1' })}
-                  onClick={() => toggle('1')}>
-                  STORIES
-                </NavLink>
-              </NavItem>
-            </Col>
-            <Col>
-              <NavItem className="tabStyle">
-                <NavLink
-                  className={classnames('Story', { active: activeTab === '2' })}
-                  onClick={() => toggle('2')}>
-                  NEWS
-                </NavLink>
-              </NavItem>
-            </Col>
-            <Col>
-              <NavItem className="tabStyle">
-                <NavLink
-                  className={classnames('Story', { active: activeTab === '3' })}
-                  onClick={() => toggle('3')}>
-                  MISC
-                </NavLink>
-              </NavItem>
-            </Col>
-          </Row>
-        </Container>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId='1'>
-          <Tab1 />
-        </TabPane>
-        <TabPane tabId='2'>
-          <h4>Render tab 2 here</h4>
-        </TabPane>
-        <TabPane tabId='3'>
-          <h4>Render tab 3 here</h4>
-
-        </TabPane>
-      </TabContent>
-    </>
->>>>>>> tabs and page styling for posts compkete
   );
 }
