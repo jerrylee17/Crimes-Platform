b0fb904bf487bfbcff9c6c065741405ebeeb94f5
Yongen Chen <yongenchen99@gmail.com>
2020-04-06 17:51:54 -0700

    rebase conflict fixed

--- src/Pages/Posts/Disp1/Misc.js
+++ src/Pages/Posts/Disp1/Misc.js
@@ -4,11 +4,7 @@ import Tab from '../../../Components/UIComponents/Tab';
 export default function Misc() {
 
   const sample = {
-<<<<<<< HEAD:src/Pages/Posts/Tab1.js
-    name: <h2><strong>STORIES</strong></h2>,
-=======
-    name: 'Misc',
->>>>>>> 36aa7fb781287e08f50d98d125a69c273993ce53:src/Pages/Posts/Disp1/Misc.js
+    name: 'STORIES',
     posts: [{
       id: 12345,
       content: 'Hello! My name is Cian and I like to eat\
